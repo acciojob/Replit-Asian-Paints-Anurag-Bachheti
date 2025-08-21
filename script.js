6454e86634508435aaf2a64e19c5ea9cbe811249
Anurag Bachheti <157354714+Anurag-Bachheti@users.noreply.github.com>
2025-08-21 16:34:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-//your JS code here. If required.
+const changeBtn = document.getElementById("change_button");
+    const resetBtn = document.getElementById("Reset");
+    const blocks = document.querySelectorAll(".grid-item");
+
+    
+    changeBtn.addEventListener("click", () => {
+      const blockId = document.getElementById("block_id").value;
+      const color = document.getElementById("colour_id").value;
+      
+    blocks.forEach(block => {
+        block.style.backgroundColor = "transparent";
+    });
+
+    if (blockId >= 1 && blockId <= 9) {
+	    document.getElementById(blockId).style.backgroundColor = color;
+    }
+});
+
+resetBtn.addEventListener("click", () => {
+      blocks.forEach(block => {
+        block.style.backgroundColor = "transparent";
+      });
+    });
+
+
